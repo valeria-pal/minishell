a6345a16c3d4d003986b9e4e042d771a9b9b6ffb
valeria-pal <vallery7543@gmail.com>
2025-09-04 18:39:12 +0200

    feature:eof handling added

--- src/promt.c
+++ src/promt.c
@@ -2,7 +2,18 @@
 
 void	promt()
 {
-	char *line = readline("minishell $");
-	printf("%s", line);
-	free(line);
+	char *line_read = readline("minishell$ ");
+	printf("%s", line_read);
+
+	if(!line_read)
+	exit(0);// or use signal SIGINT
+
+	if(*line && line_read)
+	add_history(line_read);
+
+	if(line_read)
+	{
+	free(line_read);
+	line_read = NULL;
+	}
 }
