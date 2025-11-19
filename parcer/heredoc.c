f8bc29311a57c5c3d027b37d75c11b5c4421479a
Verunchik27062003 <vpozniak@st.swps.edu.pl>
2025-11-19 15:56:13 +0100

    fixed heredoc, now works

--- parcer/heredoc.c
+++ parcer/heredoc.c
@@ -12,56 +12,89 @@
 
 #include "../include/minishell.h"
 
-static int	handle_child_heredoc(char *delimiter, int fd_temp)
+static char	*create_heredoc_filename(void)
+{
+	static unsigned int	counter;
+	char				buf[128];
+
+	if (snprintf(buf, sizeof(buf), "/tmp/minishell_hd_%d_%u.tmp",
+			(int)getpid(), counter++) < 0)
+		return (NULL);
+	return (ft_strdup(buf));
+}
+
+void	child_heredoc(char *delimiter, int fd)
 {
 	char	*line;
 
 	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
 	while (1)
 	{
-		line = readline("heredoc > ");
+		line = readline("heredoc> ");
 		if (!line || ft_strcmp(line, delimiter) == 0)
+		{
+			free(line);
 			break ;
-		write(fd_temp, line, ft_strlen(line));
-		write(fd_temp, "\n", 1);
+		}
+		write(fd, line, ft_strlen(line));
+		write(fd, "\n", 1);
 		free(line);
 	}
-	free(line);
-	close(fd_temp);
+	close(fd);
 	exit(0);
 }
 
-static int	handle_parent_heredoc(pid_t pid, int fd_temp)
+int	parent_heredoc(pid_t pid, int fd)
 {
-	int	status;
+	struct sigaction	old_int;
+	struct sigaction	old_quit;
+	struct sigaction	ignore;
+	int					status;
 
-	signal(SIGINT, SIG_IGN);
-	waitpid(pid, &status, 0);
-	close(fd_temp);
-	if (WIFSIGNALED(status))
+	ft_memset(&ignore, 0, sizeof(ignore));
+	ignore.sa_handler = SIG_IGN;
+	sigemptyset(&ignore.sa_mask);
+	sigaction(SIGINT, &ignore, &old_int);
+	sigaction(SIGQUIT, &ignore, &old_quit);
+	if (waitpid(pid, &status, 0) < 0)
+	{
+		perror("waitpid");
+		sigaction(SIGINT, &old_int, NULL);
+		sigaction(SIGQUIT, &old_quit, NULL);
+		close(fd);
 		return (1);
+	}
+	sigaction(SIGINT, &old_int, NULL);
+	sigaction(SIGQUIT, &old_quit, NULL);
+	close(fd);
+	if (WIFSIGNALED(status))
+	{
+		if (WTERMSIG(status) == SIGINT)
+			write(STDOUT_FILENO, "\n", 1);
+		return (1);
+	}
 	return (0);
 }
 
-int	heredoc(char *delimiter)
+int	heredoc_create(char *delimiter, char *outfile)
 {
-	int		fd_temp;
+	int		fd;
 	pid_t	pid;
 
-	fd_temp = open("fd_temp", O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if (fd_temp < 0)
-		return (printf("fd_temp didn't open\n"), 1);
+	if (!delimiter || !outfile)
+		return (1);
+	fd = open(outfile, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd < 0)
+		return (perror(outfile), 1);
 	pid = fork();
+	if (pid < 0)
+		return (perror("fork"), close(fd), 1);
 	if (pid == 0)
-		handle_child_heredoc(delimiter, fd_temp);
-	else if (pid > 0)
+		child_heredoc(delimiter, fd);
+	if (parent_heredoc(pid, fd))
 	{
-		if (handle_parent_heredoc(pid, fd_temp))
-			return (1);
-	}
-	else
-	{
-		perror("fork failed");
+		unlink(outfile);
 		return (1);
 	}
 	return (0);
@@ -69,23 +102,29 @@ int	heredoc(char *delimiter)
 
 int	add_heredoc_redir(t_command *cmd, t_token **tok_ptr)
 {
-	t_redirection	*redir_node;
-	char			*filename;
+	t_redirection	*node;
+	char			*file;
 	t_token			*tok;
 
+	if (!cmd || !tok_ptr || !*tok_ptr || !(*tok_ptr)->next)
+		return (0);
 	tok = *tok_ptr;
-	if (heredoc(tok->next->value) != 0)
+	file = create_heredoc_filename();
+	if (!file)
 		return (0);
-	filename = ft_strdup("fd_temp");
-	if (!filename)
+	if (heredoc_create(tok->next->value, file))
+		return (unlink(file), free(file), 0);
+	node = new_redirection(R_HEREDOC, file);
+	if (!node)
+		return (unlink(file), free(file), 0);
+	free(file);
+	if (!append_redirection(&cmd->redirs, node))
+	{
+		unlink(node->filename);
+		free(node->filename);
+		free(node);
 		return (0);
-	tok = tok->next;
-	redir_node = new_redirection(R_HEREDOC, filename);
-	free(filename);
-	if (!append_redirection(&cmd->redirs, redir_node))
-		return (0);
-	*tok_ptr = tok->next;
+	}
+	*tok_ptr = tok->next->next;
 	return (1);
 }
-
-
