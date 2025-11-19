242d2eff733830e956cb3f55e03bcd9d0b9be7bb
valeria-pal <vallery7543@gmail.com>
2025-11-19 22:12:15 +0100

    feat: create pipes function fixed

--- src/executor/pipelines/pipes.c
+++ src/executor/pipelines/pipes.c
@@ -6,13 +6,13 @@
 /*   By: vpaliash <vpaliash@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 14:21:54 by vpaliash          #+#    #+#             */
-/*   Updated: 2025/11/14 20:45:16 by vpaliash         ###   ########.fr       */
+/*   Updated: 2025/11/19 20:39:41 by vpaliash         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
 
-static int	count_pipes(t_command *cmd)
+static int	count_cmds(t_command *cmd)
 {
 	int	count;
 
@@ -24,38 +24,58 @@ static int	count_pipes(t_command *cmd)
 	}
 	return (count);
 }
-
-int (*create_pipe(int cmd_count))[2]
+void	close_pipes(int pipe_fds[][2], int last_index)
 {
-	int(*pipe_fds)[2];
-	int i;
+	last_index--;
+	while (last_index >= 0)
+	{
+		close(pipe_fds[last_index][0]);
+		close(pipe_fds[last_index][1]);
+		last_index--;
+	}
+}
+void	pipe_error_check(int pipe_fds[][2], int i)
+{
+	if (pipe(pipe_fds[i]) == -1)
+	{
+		perror("pipe");
+		close_pipes(pipe_fds, i);
+		free(pipe_fds);
+		exit(EXIT_FAILURE);
+	}
+}
+
+int (*create_pipes(t_command *cmd))[2]
+{
+	int(*pipes)[2];
 	int pipe_count;
+	int i;
 
 	i = 0;
-	pipe_count = cmd_count - 1;
+	pipe_count = count_cmds(cmd) - 1 ;
 	if (pipe_count <= 0)
 		return (NULL);
-	pipe_fds = malloc((pipe_count - 1) * sizeof(int[2]));
-	if (!pipe_fds)
+	pipes = malloc(pipe_count * sizeof(int[2]));
+	if (!pipes)
 	{
 		perror("malloc");
 		exit(EXIT_FAILURE);
 	}
-    while (i < pipe_count)     
-	{                
-        if (pipe(pipe_fds[i]) == -1)
-        {
-            perror("pipe");
-			free(pipe_fds[i]);
-            free(pipe_fds);
-            exit(EXIT_FAILURE);
-        }
-        i++;
-    }
-	return (pipe_fds);
+	while (i < pipe_count)
+	{
+		pipe_error_check(pipes, i); // creates pipe and checks for error
+		i++;
+	}
+	return (pipes);
 }
 
-void	launch_command(t_command *cmd, int i, int pipe_fds[][2],int cmd_count)
+
+void execute_pipeline(t_command * cmd, char **envp)
+{
+	
+}
+
+void	launch_command(t_command *cmd, int i, int pipe_fds[][2])
 {
 	pid_t	pid;
 
@@ -65,19 +85,18 @@ void	launch_command(t_command *cmd, int i, int pipe_fds[][2],int cmd_count)
 		perror("fork");
 		exit(EXIT_FAILURE);
 	}
-	if (pid == 0)
+	if (pid == 0) // child process
 	{
 		if (cmd->next)
 		{
 			dup2(pipe_fds[i][1], STDOUT_FILENO);
 			close(pipe_fds[i][0]);
-		}	
+		}
 		if (i > 0)
 		{
 			dup2(pipe_fds[i - 1][0], STDIN_FILENO);
 			close(pipe_fds[i - 1][1]);
 		}
-		
 		if (apply_redirections_to_cmd(cmd) < 0)
 			exit(EXIT_FAILURE);
 		if (execve(cmd->argv[0], cmd->argv, NULL) < 0)
@@ -85,22 +104,5 @@ void	launch_command(t_command *cmd, int i, int pipe_fds[][2],int cmd_count)
 			perror("execve");
 			exit(EXIT_FAILURE);
 		}
-		
-	}
-}
-
-void	execute_pipes(t_command *cmd, char *const envp[])
-{
-	int pipe_count;
-	t_command *head = cmd;
-	int i;
-
-	pipe_count = count_pipes(cmd);
-
-	int pipe_fds[pipe_count - 1];
-	while (i < pipe_count)
-	{
-		launch_command(cmd, i, pipe_fds);
-		cmd = cmd->next;
 	}
 }
